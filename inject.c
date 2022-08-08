d1e995ca2c4576a09325483d475bb198e6c289f3
Ethan SAFAR <esafar@e1r11p17.clusters.42paris.fr>
2022-08-08 17:43:35 +0200

    saving file

--- inject.c
+++ inject.c
@@ -6,7 +6,7 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 17:24:19 by esafar            #+#    #+#             */
-/*   Updated: 2022/08/08 17:30:14 by esafar           ###   ########.fr       */
+/*   Updated: 2022/08/08 17:43:08 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 #undef malloc
 
 void    *malloc(size_t size){
+    (void)size;
     write(1, "test\n", 5);
     return (NULL);
 }
