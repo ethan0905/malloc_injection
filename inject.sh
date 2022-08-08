280ff7f7e2eda85366a5eca38f4f7fe90bf4c337
Ethan SAFAR <esafar@e1r11p17.clusters.42paris.fr>
2022-08-08 17:50:01 +0200

    added bash

--- inject.sh
+++ inject.sh
@@ -6,9 +6,11 @@
 #    By: esafar <esafar@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/08/08 17:26:55 by esafar            #+#    #+#              #
-#    Updated: 2022/08/08 17:36:01 by esafar           ###   ########.fr        #
+#    Updated: 2022/08/08 17:49:38 by esafar           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+
+#!/bin/bash
 
 eval path=$PWD
 echo $path/malloc.so
